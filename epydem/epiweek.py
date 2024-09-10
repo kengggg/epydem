f34414bc4e69d861c6e48b3debf6084e92744e96
Keng Susumpow <patipat@me.com>
2024-09-10 13:19:42 +0700

    Refactor calculate_epi_week function to use a more descriptive function name and validate date string format

--- epydem/epiweek.py
+++ epydem/epiweek.py
@@ -2,6 +2,23 @@ import re
 from datetime import datetime, timedelta
 
 def calcuate(date_str):
+  """
+  Calculate the epidemiological week number for a given date string.
+
+  This function determines the week number of the year for a given date
+  based on epidemiological weeks, where the first epidemiological week
+  starts on a Sunday and includes the first Thursday of the year.
+
+  Args:
+    date_str (str): The date in 'YYYY-MM-DD' format.
+
+  Returns:
+    int: The epidemiological week number. Returns 0 if the date is
+    before the start of the epidemiological year.
+
+  Raises:
+    ValueError: If the provided date string does not match the 'YYYY-MM-DD' format.
+  """
   if _verify_date_str(date_str):
     date = datetime.strptime(date_str, '%Y-%m-%d')
     year = date.year
@@ -16,6 +33,18 @@ def calcuate(date_str):
       return (date - week_start).days // 7
 
 def _verify_date_str(date_str):
+  """
+  Verify that the date string is in the 'YYYY-MM-DD' format.
+
+  Args:
+    date_str (str): The date string to verify.
+
+  Returns:
+    bool: True if the date string is in the correct format.
+
+  Raises:
+    ValueError: If the date string does not match the 'YYYY-MM-DD' format.
+  """
   date_pattern = r'^\d{4}-\d{2}-\d{2}$'  # Pattern for YYYY-MM-DD
 
   if not re.match(date_pattern, date_str):
